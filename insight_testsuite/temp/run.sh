117e7df1c609b28ff11cbb9a5fd81310bacce6c6
pujithareddy554 <pujithareddys@mail.fresnostate.edu>
2020-02-20 13:20:13 -0800

    Second commit

--- insight_testsuite/temp/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) the code and then execute it.
-python ./src/border_analytics.py --input ./input/Border_Crossing_Entry_Data.csv --output ./output/report.csv
