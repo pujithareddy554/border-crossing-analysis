d922d4647afb50253bdb01d91b47c69277607036
pujithareddy554 <pujithareddys@mail.fresnostate.edu>
2020-02-20 12:53:21 -0800

    Second commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) the code and then execute it.
-# python ./src/border_analytics.py --input ./input/Border_Crossing_Entry_Data.csv --output ./output/report.csv
+# python3 ./src/border_analytics.py --input ./input/Border_Crossing_Entry_Data.csv --output ./output/report.csv
